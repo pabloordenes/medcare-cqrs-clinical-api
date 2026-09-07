fdd8ed6d4a55478ad6ca8e787962438089754942
Pablo <pabloinfortech@gmail.com>
2026-09-07 19:34:48 -0300

    feat(domain): implementación de la entidad ScheduleBlock con validaciones temporales

---
+++ MedCareOS.Domain/Entities/ScheduleBlock.cs
@@ -0,0 +1,40 @@
+﻿namespace MedCareOS.Domain.Entities;
+
+public class ScheduleBlock
+{
+    public Guid Id { get; private set; }
+    public Guid DoctorId { get; private set; }
+    public Guid BoxId { get; private set; }
+    public DateTime StartTime { get; private set; }
+    public DateTime EndTime { get; private set; }
+
+    private ScheduleBlock() {} // ef
+
+    private ScheduleBlock(Guid id,
+        Guid doctorId, Guid boxId,
+        DateTime startTime, DateTime endTime)
+    {
+        Id = id;
+        DoctorId = doctorId;
+        BoxId = boxId;
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+
+    public static ScheduleBlock Create(Guid doctorId, Guid boxId, DateTime startTime, DateTime endTime)
+    {
+        if (startTime >= endTime)
+            throw new ArgumentException("No puedes terminar un turno antes de empezarlo.");
+        
+        if (doctorId == Guid.Empty || boxId == Guid.Empty)
+            throw new ArgumentException("Debes seleccionar un doctor/box.");
+        
+        return new ScheduleBlock(
+            Guid.NewGuid(),
+            doctorId,
+            boxId,
+            startTime,
+            endTime
+            );
+    }
+}
