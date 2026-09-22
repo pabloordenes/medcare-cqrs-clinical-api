37070e9cb130cf6ee361b8ced620fe082313b68b
Pablo <pabloinfortech@gmail.com>
2026-09-22 12:24:27 -0300

    refactor: actualizar paciente con atributos de contratos

--- MedCareOS.Domain/Entities/Patient.cs
+++ MedCareOS.Domain/Entities/Patient.cs
@@ -3,57 +3,71 @@
 public class Patient
 {
     public Guid Id { get; private set; }
+    public Guid UserId { get; set; }
     public string Rut { get; private set; } = string.Empty;
     public string FirstName { get; private set; } = string.Empty;
     public string LastName { get; private set; } = string.Empty;
-    public string MedicalInsurance { get; private set; } = string.Empty;
+    public DateTime DateOfBirth { get; private set; }
+    public string Gender { get; private set; } = string.Empty;
+    public string? Address { get; private set; }
+    public string? Neighborhood { get; private set; }
     public string Phone { get; private set; } = string.Empty;
     public string? Email { get; private set; }
     public int NoShowHistoryCount { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; }
 
-    private Patient() // para reflection con ef
-    {
-        
-    }
+    private Patient() { } // ef core
 
-    private Patient(Guid id, string rut, 
-        string firstName, string lastName, 
-        string medicalInsurance, string phone, 
+    private Patient(Guid id, Guid userId, string rut, 
+        string firstName, string lastName, DateTime dateOfBirth,
+        string gender, string address, string neighborhood, string phone, 
         string? email, int noShowHistoryCount)
     {
         Id = id;
+        UserId = userId;
         Rut = rut;
         FirstName = firstName;
         LastName = lastName;
-        MedicalInsurance = medicalInsurance;
+        DateOfBirth = dateOfBirth;
+        Gender = gender;
         Phone = phone;
         Email = email;
+        Address =  address;
+        Neighborhood = neighborhood;
         NoShowHistoryCount = noShowHistoryCount;
+        CreatedAt = DateTimeOffset.UtcNow;
     }
     
-    public static Patient Create(Guid id, string rut, string firstName, 
-        string lastName, string medicalInsurance, string phone, string? email = null)
+    public static Patient Create(Guid userId, string rut, string firstName, 
+        string lastName, DateTime dateOfBirth, string gender, string phone, string? email = null,
+        string? address = null, string? neighborhood = null)
     {
+        if (userId == Guid.Empty)
+            throw new ArgumentException("Debe vincularse a un usuario de Supabase Auth.");
         if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
             throw new ArgumentException("Ambos nombres deben llenarse.");
+
+        if (string.IsNullOrWhiteSpace(gender))
+            throw new ArgumentException("Debe especificar su genero.");
         
         if (string.IsNullOrWhiteSpace(rut))
             throw new ArgumentException("Debe ingresar su RUT.");
         
         if (string.IsNullOrWhiteSpace(phone))
             throw new ArgumentException("Debe ingresar su Numero Telefonico.");
-        
-        if (string.IsNullOrWhiteSpace(medicalInsurance))
-            throw new ArgumentException("Debe especificar su seguro de salud.");
 
         return new Patient(
-            id,
+            Guid.NewGuid(),
+            userId,
             rut,
             firstName,
             lastName,
-            medicalInsurance, 
+            dateOfBirth,
+            gender,
             phone,
             email,
+            address,
+            neighborhood,
             0);
     }
 
