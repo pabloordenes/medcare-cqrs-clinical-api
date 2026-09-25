3a1757740d4fd44cdda888ec6411789d0b61a191
Pablo <pabloinfortech@gmail.com>
2026-09-25 00:53:17 -0300

    feat(application): command y handler para iniciar cita

---
+++ MedCareOS.Application/Appointments/Commands/StartConsultation/StartConsultationCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace MedCareOS.Application.Appointments.Commands.StartConsultation;
+
+public record StartConsultationCommand(
+    Guid AppointmentId) : IRequest;
---
+++ MedCareOS.Application/Appointments/Commands/StartConsultation/StartConsultationCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MedCareOS.Domain.Repositories;
+using MediatR;
+
+namespace MedCareOS.Application.Appointments.Commands.StartConsultation;
+
+public class StartConsultationCommandHandler : IRequestHandler<StartConsultationCommand>
+{
+    private readonly IAppointmentRepository _appointmentRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public StartConsultationCommandHandler(IAppointmentRepository appointmentRepository, IUnitOfWork unitOfWork)
+    {
+        _appointmentRepository = appointmentRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(StartConsultationCommand request, CancellationToken cancellationToken)
+    {
+        var appointment = await _appointmentRepository.GetAppointmentByIdAsync(request.AppointmentId, cancellationToken);
+        
+        if (appointment is null)
+            throw new Exception($"La cita con id {request.AppointmentId} no se encuentra.");
+        
+        appointment.StartConsultation();
+        
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
