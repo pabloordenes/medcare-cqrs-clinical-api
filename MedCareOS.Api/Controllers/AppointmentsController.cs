dff5afff82e2c549eaef0264f33fd6378ee96a4c
Pablo <pabloinfortech@gmail.com>
2026-09-25 01:16:15 -0300

    feat(api): implementar metodo para evaluar estados y delegar a commands con mediatr

--- MedCareOS.Api/Controllers/AppointmentsController.cs
+++ MedCareOS.Api/Controllers/AppointmentsController.cs
@@ -1,4 +1,6 @@
 ﻿using MedCareOS.Application.Appointments.Commands.CreateAppointment;
+using MedCareOS.Application.Appointments.Commands.FinishConsultation;
+using MedCareOS.Application.Appointments.Commands.StartConsultation;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -23,5 +25,28 @@ public class AppointmentsController : ControllerBase
     {
         var appointmentId = await _sender.Send(command, cancellationToken);
         return Ok(appointmentId);
+    }
+    
+    // dto simple
+    public record UpdateAppointmentStatusRequestDto(string Estado);
+
+    [HttpPatch("{id:guid}/estado")]
+    public async Task<IActionResult> UpdateAppointmentStatusRequest([FromRoute] Guid id, 
+        [FromBody] UpdateAppointmentStatusRequestDto request, CancellationToken cancellationToken)
+    {
+        switch (request.Estado.ToLower())
+        {
+            case "en_curso":
+                await _sender.Send(new StartConsultationCommand(id), cancellationToken);
+                break;
+            case "finalizada":
+                await _sender.Send(new FinishConsultationCommand(id), cancellationToken);
+                break;
+            // todo: agregar el resto de transiciones
+            default:
+                return BadRequest("El estado no es válido o aún no está soportado.");
+        }
+        
+        return NoContent();
     }
 }
