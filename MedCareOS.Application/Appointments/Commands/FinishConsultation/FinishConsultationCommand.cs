b299ec895a639359582ea942d0d0c4ee230b90ec
Pablo <pabloinfortech@gmail.com>
2026-09-25 01:02:00 -0300

    feat(application): agregar command y handler para dar cita por finalizada

---
+++ MedCareOS.Application/Appointments/Commands/FinishConsultation/FinishConsultationCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace MedCareOS.Application.Appointments.Commands.FinishConsultation;
+
+public record FinishConsultationCommand(
+    Guid AppointmentId) : IRequest;
---
+++ MedCareOS.Application/Appointments/Commands/FinishConsultation/FinishConsultationCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MedCareOS.Domain.Repositories;
+using MediatR;
+
+namespace MedCareOS.Application.Appointments.Commands.FinishConsultation;
+
+public class FinishConsultationCommandHandler : IRequestHandler<FinishConsultationCommand>
+{
+    private readonly IAppointmentRepository _appointmentRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public FinishConsultationCommandHandler(IAppointmentRepository appointmentRepository, IUnitOfWork unitOfWork)
+    {
+        _appointmentRepository = appointmentRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(FinishConsultationCommand request, CancellationToken cancellationToken)
+    {
+        var appointment = await _appointmentRepository.GetAppointmentByIdAsync(request.AppointmentId);
+        
+        if (appointment is null)
+            throw new Exception("Cita no encontrada."); // TODO: crear excepcion NotFoundException personalizada
+        
+        appointment.FinishConsultation();
+        
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
